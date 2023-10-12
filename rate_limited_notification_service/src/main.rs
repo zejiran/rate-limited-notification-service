e9691ffa5e1b8b573ecbc33a1736de4ea4f5713b
Juan Alegría <juanszalegria@gmail.com>
2023-10-12 14:04:28 -0500

    Fix: use limit rate by user instead of global counters

--- rate_limited_notification_service/src/main.rs
+++ rate_limited_notification_service/src/main.rs
@@ -7,8 +7,12 @@ struct NotificationService {
 
 struct RateLimit {
     max_requests: u32,
-    allowed_requests: u32,
     per_duration: Duration,
+    recipient_counters: HashMap<String, RecipientCounter>,
+}
+
+struct RecipientCounter {
+    allowed_requests: u32,
     last_request: Option<Instant>,
 }
 
@@ -22,9 +26,8 @@ impl NotificationService {
     fn create_rate_limit(max_requests: u32, per_duration: Duration) -> RateLimit {
         RateLimit {
             max_requests,
-            allowed_requests: max_requests,
             per_duration,
-            last_request: Some(Instant::now()),
+            recipient_counters: HashMap::new(),
         }
     }
 
@@ -34,31 +37,33 @@ impl NotificationService {
         recipient: &str,
         message: &str,
     ) -> Result<(), String> {
-        if let Some(rate_limit) = self.rate_limits.get_mut(notification_type) {
-            let per_duration = rate_limit.per_duration;
-            let now = Instant::now();
-            let elapsed = now.duration_since(rate_limit.last_request.unwrap_or(now));
+        let rate_limit = self
+            .rate_limits
+            .entry(notification_type.to_string())
+            .or_insert(Self::create_rate_limit(u32::MAX, Duration::from_secs(1)));
 
-            if elapsed < per_duration {
-                if rate_limit.allowed_requests == 0 {
-                    return Err(format!(
-                        "Rate limit exceeded for {} notifications. No more requests allowed.",
-                        notification_type
-                    ));
-                }
-                // Update the number of allowed requests if it's not zero
-                rate_limit.allowed_requests = rate_limit.allowed_requests.saturating_sub(1);
-            } else {
-                // Reset the rate limit if the duration has passed
-                rate_limit.last_request = Some(now);
-                rate_limit.allowed_requests = rate_limit.max_requests;
+        let recipient_counter = rate_limit
+            .recipient_counters
+            .entry(recipient.to_string())
+            .or_insert(RecipientCounter {
+                allowed_requests: rate_limit.max_requests,
+                last_request: Some(Instant::now()),
+            });
+
+        let now = Instant::now();
+        let elapsed = now.duration_since(recipient_counter.last_request.unwrap_or(now));
+
+        if elapsed <= rate_limit.per_duration {
+            if recipient_counter.allowed_requests <= 0 {
+                return Err(format!(
+                    "Rate limit exceeded for {} notifications to {}. No more requests allowed.",
+                    notification_type, recipient
+                ));
             }
+            recipient_counter.allowed_requests -= 1;
         } else {
-            // If no rate limit rule is defined, treat it as no rate limiting.
-            self.rate_limits.insert(
-                notification_type.to_string(),
-                Self::create_rate_limit(u32::MAX, Duration::from_secs(1)),
-            );
+            recipient_counter.last_request = Some(now);
+            recipient_counter.allowed_requests = rate_limit.max_requests - 1;
         }
 
         // Perform the actual notification sending here.
@@ -90,7 +95,15 @@ fn main() {
 
     // Example usage:
     for _ in 0..5 {
-        match service.send("news", "user", "This is a news update") {
+        match service.send("news", "user 1", "This is a news update") {
+            Ok(()) => println!("Notification sent successfully."),
+            Err(err) => println!("Failed to send notification: {}", err),
+        }
+        match service.send("marketing", "user 1", "This is a marketing update") {
+            Ok(()) => println!("Notification sent successfully."),
+            Err(err) => println!("Failed to send notification: {}", err),
+        }
+        match service.send("news", "user 2", "This is a news update") {
             Ok(()) => println!("Notification sent successfully."),
             Err(err) => println!("Failed to send notification: {}", err),
         }
